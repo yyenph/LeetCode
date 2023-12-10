c7a55ab47dd6c88e120b63fa2e65efd50c0de642
Yen Pham <117415650+yyenph@users.noreply.github.com>
2023-12-09 21:13:19 -0500

    Time: 19 ms (65.50%), Space: 13.6 MB (10.37%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.py
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.py
@@ -1,19 +1,34 @@
 class Solution(object):
     def uniqueOccurrences(self, arr):
-        occurence_cnt = []
-        sorted_arr = sorted(arr)
-        i,count = 0,1
-        while i < len(arr):
-            if i == len(arr) -1:
-                if count in occurence_cnt:
-                    return False
-            elif sorted_arr[i] != sorted_arr[i+1]:
-                if count in occurence_cnt:
-                    return False
-                occurence_cnt.append(count)
-                count = 1
+        #Solution 1, nlogn because of sorting
+        # occurence_cnt = []
+        # sorted_arr = sorted(arr)
+        # i,count = 0,1
+        # while i < len(arr):
+        #     if i == len(arr) -1:
+        #         if count in occurence_cnt:
+        #             return False
+        #     elif sorted_arr[i] != sorted_arr[i+1]:
+        #         if count in occurence_cnt:
+        #             return False
+        #         occurence_cnt.append(count)
+        #         count = 1
+        #     else:
+        #         count += 1
+        #     i += 1
+        # return True
+        #Solution 2, using hashmap and set to compare the count value. Time: O(n)
+        occurence_cnt = {}
+        for num in arr:
+            occurence_cnt[num] = occurence_cnt.get(num, 0) + 1
+            
+        unique_count = set()
+        for count in occurence_cnt.values():
+            if count in unique_count:
+                return False
             else:
-                count += 1
-            i += 1
+                unique_count.add(count)
         return True
+                
+        
                 
