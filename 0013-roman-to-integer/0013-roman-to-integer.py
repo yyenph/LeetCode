a545624035971087620660531c2c4bf2654bfacd
Yen Pham <117415650+yyenph@users.noreply.github.com>
2023-12-17 21:58:22 -0500

    Time: 25 ms (75.91%), Space: 13.3 MB (72.16%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def romanToInt(self, s):
+
+        # dict={'M':1000,'CM':900,'D':500,'CD':400,'C':100,'XC':90,'L':50,'XL':40,'X':10,'IX':9,'V':5,'IV':4,'I':1}
+        # result=0
+        # s=s.replace('IV','IIII').replace('IX','VIIII').replace('XL','XXXX').replace('XC','LXXXX').replace('CD','CCCC').replace('CM','DCCCC')
+        # for char in s:
+        #     result+=dict[char]
+        # return result
+        roman = {'M': 1000,'D': 500 ,'C': 100,'L': 50,'X': 10,'V': 5,'I': 1}
+        z = 0
+        for i in range(0, len(s) - 1):
+            if roman[s[i]] < roman[s[i+1]]:
+                z -= roman[s[i]]
+            else:
+                z += roman[s[i]]
+        return z + roman[s[-1]]
