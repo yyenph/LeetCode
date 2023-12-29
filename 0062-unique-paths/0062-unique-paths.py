ea2cc5a250229894f6dc4989cd64b0b4ab15710e
Yen Pham <117415650+yyenph@users.noreply.github.com>
2023-12-28 22:20:47 -0500

    Time: 11 ms (83.21%), Space: 13.2 MB (62.14%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,8 +1,8 @@
 class Solution(object):
     def uniquePaths(self, m, n):
        #m is row, n is col
-        row = [0] * n
-        for num in range(m):
+        row = [1] * n
+        for num in range(m-1):
             next_row=[1]*n #temporary value before computing
             for ind in range(n-2,-1,-1):
             #last index has 1 way to go, so it stay as 1, also to avoid out of index for the following line (using val in row and the one after to calculate number of way)
