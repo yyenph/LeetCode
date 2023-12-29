a0147587d234cc3c5327026836e9c22cdb0ec263
Yen Pham <117415650+yyenph@users.noreply.github.com>
2023-12-28 22:20:01 -0500

    Time: 16 ms (48.50%), Space: 13.2 MB (85.99%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -8,7 +8,6 @@ class Solution(object):
             #last index has 1 way to go, so it stay as 1, also to avoid out of index for the following line (using val in row and the one after to calculate number of way)
                 next_row[ind]=row[ind] + next_row[ind + 1]
             row = next_row
-            print(num,row)
         return row[0]
         
         
