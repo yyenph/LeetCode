aa1dcabcb237f54e1c6a3a7c59d0494c6480e490
Yen Pham <117415650+yyenph@users.noreply.github.com>
2023-12-30 15:06:14 -0500

    Time: 25 ms (6.71%), Space: 13.5 MB (17.05%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
@@ -9,19 +9,37 @@ class Solution(object):
     def rightSideView(self, root):
         if root is None:
             return []
-        queue = deque([root])
+        #Solution 1
+        # queue = deque([root])
+        # result = []
+        # last_node = None
+        # while queue:
+        #     levels= len(queue)
+        #     #keep popleft until the farright node become last node
+        #     for level in range(levels):
+        #         current_node = queue.popleft()
+        #         last_node = current_node.val
+        #         if current_node.left:
+        #             queue.append(current_node.left)
+        #         if current_node.right:
+        #             queue.append(current_node.right)
+        #     result.append(last_node)
+        # return result
+        #-------------
+        #Solution 2, optimize to not calculate len(queue) inside the loop
+        
+        queue = deque([(root,0)])
         result = []
-        last_node = None
         while queue:
-            levels= len(queue)
-            for level in range(levels):
-                current_node = queue.popleft()
-                last_node = current_node.val
-                if current_node.left:
-                    queue.append(current_node.left)
-                if current_node.right:
-                    queue.append(current_node.right)
-            result.append(last_node)
+            current,level = queue.popleft()
+            if level == len(result):
+                if not queue:
+                    result.append(current.val)
+                elif queue[0][1] != level:
+                    result.append(current.val)
+            if current.left:
+                queue.append((current.left,level+1))
+            if current.right:
+                queue.append((current.right,level+1))
+
         return result
-        
-        
